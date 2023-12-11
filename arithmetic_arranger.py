8324c0584e1ca607bf2af17087c9ff5f8334e642
felix <vladimir.roldan@gmail.com>
2023-12-11 15:55:24 -0500

    testing ok

--- arithmetic_arranger.py
+++ arithmetic_arranger.py
@@ -8,14 +8,14 @@ def arithmetic_arranger(problems, showresult = False):
             try:
                 result = (int(pList[0]) + int(pList[2]))
             except ValueError as verr:
-                return "Error: Numbers must only contain digits"
+                return "Error: Numbers must only contain digits."
         elif (pList[1] == '-'):
             try:
                 result = (int(pList[0]) - int(pList[2]))
             except ValueError as verr:
-                return "Error: Numbers must only contain digits"
+                return "Error: Numbers must only contain digits."
         else:
-            return "Error: Operator must be '+' or '-'"
+            return "Error: Operator must be '+' or '-'."
         pList.append(str(result))
         problemsAsList.append(pList)
     nums = ''
@@ -23,22 +23,32 @@ def arithmetic_arranger(problems, showresult = False):
     ops = ''
     res = ''
     sep =''
+    espace = ''
+    i= 0
     for p in problemsAsList:
+        if(i>0):
+            espace = '    '
         maxL = len(p[0])
         if len(p[2]) > len(p[0]):
             maxL = len(p[2])
+        
         if len(p[0]) < 5:
-            nums += (p[0].rjust(6,' '))+('    ')
+            nums+= espace
+            nums += (p[0].rjust(maxL+2,' '))
         else:
-            return 'Error: Numbers cannot be more than four digits'
-        dens += (p[1])
+            return 'Error: Numbers cannot be more than four digits.'
         if len(p[2]) < 5:
-            dens += (p[2].rjust(5,' '))+('    ')
+            dens += espace
+            dens += (p[1])
+            dens += (p[2].rjust(maxL+1,' '))
         else:
-            return 'Error: Numbers cannot be more than four digits'
+            return 'Error: Numbers cannot be more than four digits.'
         #ops += (p[1])
-        print(maxL)
-        sep += '    '.rjust(maxL+2,'-')
-        res += (p[3].rjust(maxL+2,' '))+('    ')
-    arranged_problems = f"{nums}\n{dens}\n{sep}\n{res}" if showresult else f"{nums}\n{dens}\n{sep}"
+        #print(maxL)
+        sep += espace
+        sep += ''.rjust(maxL+2,'-')
+        res += espace
+        res += (p[3].rjust(maxL+2,' '))
+        i = i+1
+    arranged_problems = f"{nums}\n" +f"{dens}\n" + f"{sep}\n"+ f"{res}" if showresult else f'{nums}\n'+f'{dens}\n'+f'{sep}'
     return arranged_problems
